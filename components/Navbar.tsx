9837307cfee7c085cf6de700a33386b76eb666d8
matews-sousa <mateussdamiano@gmail.com>
2022-04-17 21:30:50 -0300

    added top colored blur and added navbar hover

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,10 +4,16 @@ const Navbar = () => {
   return (
     <nav className="fixed inset-x-0 top-6 z-30 mx-auto hidden max-w-3xl px-2 sm:block">
       <div className="flex items-center justify-center space-x-6 rounded-md bg-white/[3%] py-6 shadow-xl backdrop-blur backdrop-filter">
-        <a>home</a>
-        <a>about me</a>
-        <a>projects</a>
-        <a>contact</a>
+        <a className="cursor-pointer font-medium hover:text-blue-500">home</a>
+        <a className="cursor-pointer font-medium hover:text-blue-500">
+          about me
+        </a>
+        <a className="cursor-pointer font-medium hover:text-blue-500">
+          projects
+        </a>
+        <a className="cursor-pointer font-medium hover:text-blue-500">
+          contact
+        </a>
       </div>
     </nav>
   );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body className="bg-neutral-900">
+        <div className="absolute inset-x-0 top-[-2vh] mx-auto h-20 max-w-2xl bg-gradient-to-r from-blue-900 to-blue-400 blur-3xl filter"></div>
         <Main />
         <NextScript />
       </body>
