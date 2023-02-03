f349904daef86871f5c15d3e116d15973fb6e4c1
Mateus Sousa <mateussdamiano@gmail.com>
2023-02-03 14:31:52 -0300

    change style of contact section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -68,8 +68,8 @@ export default function Home({ projects }: Props) {
         <div className="grid grid-cols-3">
           <div className="space-y-4">
             <div>
-              <p className="mb-1 text-sm font-semibold text-neutral-500">
-                Email:
+              <p className="text-sm font-semibold uppercase tracking-widest text-blue-500">
+                Email
               </p>
               <a
                 href="mailto:mateussdamiano@gmail.com"
@@ -79,8 +79,8 @@ export default function Home({ projects }: Props) {
               </a>
             </div>
             <div>
-              <p className="mb-1 text-sm font-semibold text-neutral-500">
-                LinkedIn:
+              <p className="text-sm font-semibold uppercase tracking-widest text-blue-500">
+                LinkedIn
               </p>
               <a
                 href="https://www.linkedin.com/in/mateus-damiano/"
@@ -123,6 +123,7 @@ export default function Home({ projects }: Props) {
                 name="name"
                 className="rounded-md border border-neutral-600 p-2 focus:border-blue-400 focus:outline-none dark:bg-neutral-800"
                 placeholder="Your message..."
+                rows={5}
               />
             </div>
             <button className="col-span-2 ml-auto flex items-center gap-1 rounded-md bg-blue-400 py-2 px-4 font-semibold text-white hover:bg-blue-600">
